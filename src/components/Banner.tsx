f571f4c72004580ecf9be803f3ad1214538faca4
tomo-no-kai <kumanote.2tu@gmail.com>
2025-07-23 11:37:47 +0900

    バナー幅変更

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,9 +11,9 @@ export default function Banner({ text }: BannerProps) {
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: "tween", stiffness: 100, damping: 12 }}
-      className="fixed top-0 left-0 w-full bg-white py-7 text-center text-base font-medium rounded-b-[80px] rounded-t-none shadow-xl z-50"
+      className="fixed top-0 left-0 w-full bg-white py-9 text-center text-base font-medium rounded-b-[80px] rounded-t-none shadow-xl z-50"
     >
-      <span className="block mt-7">{text}</span>
+      <span className="block mt-14">{text}</span>
     </motion.div>
   );
 }
