11295d50b4907bec3229413a164f9b8eb85cc820
tomo-no-kai <kumanote.2tu@gmail.com>
2025-09-01 06:34:37 +0900

    画像固定

--- src/components/BackgroundWrapper.tsx
+++ src/components/BackgroundWrapper.tsx
@@ -8,8 +8,10 @@ export default function BackgroundWrapper({ children }: Props) {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* 背景 */}
-      <div className="absolute inset-0 -z-10 bg-[url('/back.png')] bg-contain bg-top bg-no-repeat bg-fixed" />
-
+      <div
+      className="absolute inset-0 -z-10 bg-[url('/back.png')] bg-cover bg-top bg-no-repeat"
+      style={{ width: "100%", height: "100vh", minHeight: "100%", objectFit: "cover" }}
+      />
       {/* 中身 */}
       {children}
     </div>
